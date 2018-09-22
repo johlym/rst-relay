00e54356bf21a673885ed029878474e503585a76
Johnathan Lyman <jelyman@gmail.com>
2018-09-22 15:17:54 -0700

    A more reliable way to do report

--- app/jobs/collect_and_forward_job.rb
+++ app/jobs/collect_and_forward_job.rb
@@ -15,14 +15,11 @@ class CollectAndForwardJob < ApplicationJob
   def process_and_forward_json_object(j)
     hgkey = "2ef65f36-c77e-4024-a895-d486fd7c11f2"
     conn = TCPSocket.new '3de08a3c.carbon.hostedgraphite.com', 2003
-    ['tok', 'lan', 'gra', 'fra', 'bhs', 'sin', 'mia', 'syd'].each do |z|
-      timing_value = j[z]["timings"]["total"]
-      puts "Reporting: Zone #{z} has value: #{timing_value}"
-      # Keen.publish(:response_times, {
-      #   :location => z,
-      #   :value => timing_value
-      # })
-      conn.puts "#{hgkey}.response_times.#{z} #{timing_value}\n"
+    puts "Received #{j.count} samples to report"
+    j.each do |z|
+      zone = z[0]
+      timing = z[1]['timings']['total']
+      conn.puts "#{hgkey}.response_times.#{zone} #{timing}\n"
     end
     conn.close
   end
