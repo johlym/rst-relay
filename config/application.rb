5d57feb3cc1fa1870a0c9e87fac8bc1af4ec88e9
Johnathan Lyman <jelyman@gmail.com>
2018-09-23 19:49:09 -0700

    change log level to info

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module ResponseTimes
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.log_level = :info
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
